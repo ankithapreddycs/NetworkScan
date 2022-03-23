08b670dae11ea39fbffe8e50f08231689fc07d4a
ankithapreddycs <ankitha@connectivitysolutions.in>
2022-03-24 01:05:44 +0530

    ping and traceroute

--- src/PingSweep.groovy
+++ src/PingSweep.groovy
@@ -1,21 +1,41 @@
 
 
 class PingSweeper {
-	static void main(String[] args){
-		String command = "ping 172.217.31.206"
-		try{
-			Process process = Runtime.getRuntime().exec(command)
-			BufferedReader reader = new BufferedReader(
-					new InputStreamReader(process.getInputStream()))
-			String line;
-			while ((line = reader.readLine()) != null){
-				System.out.println(line)
-			}
-			reader.close()
+	static boolean ping(String ip) throws UnknownHostException, IOException {
+		boolean state = false
+		try {
+			InetAddress inet = InetAddress.getByName(ip)
+			state = inet.isReachable(1000)
+		}catch (UnknownHostException e){
+			e.printStackTrace()
 		}catch(IOException e){
 			e.printStackTrace()
 		}
+		return state
+	}
+	static void Traceroute(boolean state, String command){
+		if (state ) {
+			try {
+				Process process = Runtime.getRuntime().exec(command)
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(process.getInputStream()))
+				String line
+				while ((line = reader.readLine()) != null) {
+					System.out.println(line)
+				}
+				reader.close()
+			} catch (IOException e) {
+				e.printStackTrace()
+			}
+		}else{
+			println("Host Not Reachable")
+		}
 	}
 
-
 }
+	static void main(String[] args){
+		String ip = "52.89.84.245"
+		String command = "traceroute 52.89.84.245"
+		boolean state = PingSweeper.ping(ip)
+		PingSweeper.Traceroute(state, command)
+	}
